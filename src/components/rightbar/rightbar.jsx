121bba8fa6179371e8f5380af37a0f1c86ed90f9
Thomas Oduor <oduorthomas14.com>
2023-04-14 07:41:59 -0400

    Initial rightbar component

--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -1,5 +1,53 @@
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="assets/gift.png" alt="AD" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Tom</b> and <b>3 others</b> have birthdays today
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="Img" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/prof3.jpg"
+                alt="img"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Oduor Tom</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/prof3.jpg"
+                alt="img"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Oduor Tom</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/prof3.jpg"
+                alt="img"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Oduor Tom</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }

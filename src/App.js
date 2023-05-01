b77bb859588c657d09bc6235f0870b849d65af15
Thomas Oduor <oduorthomas14.com>
2023-05-01 05:48:47 -0400

    added different paths

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import Home from "./pages/home/home";
-// import Profile from "./pages/profile/profile";
-// import Login from "./pages/login/login";
-// import Register from "./pages/register/register";
+import Profile from "./pages/profile/profile";
+import Login from "./pages/login/login";
+import Register from "./pages/register/register";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="profile/:username" element={<Profile />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
